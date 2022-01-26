7783bdc37655a43288819b6454aac55819bdd2e9
Jackroot302 <98463093+Jackroot302@users.noreply.github.com>
2022-01-26 17:18:05 +0500

    Create Jack.py
    git clone https://github.com/Jackroot302/jack.git
    ls
    cd Jack
    python2 Jack.py

---
+++ Jack.py
@@ -0,0 +1,29 @@
+##### Made With By : [Moch Aang Ardiansyah-XD](url)
+#### Install Script On Termux
+$ _`pkg update && pkg upgrade`_
+
+$ _`pkg install python git`_
+
+$ _`pip install requests mechanize`_
+
+$ _`pip install futures bs4`_
+
+$ _`rm -rf FreeCrack`_
+
+$ _`git clone https://github.com/AngCyber/FreeCrack`_
+
+$ _`cd FreeCrack`_
+
+$ _`ls`_
+
+$ _`python free.py`_
+
+#### Menu & Fitur Crack
+![IMG_20211231_044515_e](https://user-images.githubusercontent.com/92802033/150424935-7ab12d9f-be56-47e6-84bf-58e808a9529f.png)
+
+#### Donasi
+- [ ] Dana : [083806858479](url)
+- [ ] Pulsa : [081392979518](url)
+#### Contact Me
+- [ ] Facebook  : [https://www.facebook.com/jack.root.official.account.549](url)
+- [ ] WhatsApp : [+923440648813](url)
